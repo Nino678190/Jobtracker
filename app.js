6a26a2dc3e8472cdce39c3c6b5f35101fe2bd9fb
Nino678190 <superwauzi@gmx.de>
2025-11-07 21:59:26 +0100

    feat: Update application logic and enhance user authentication flow

--- app.js
+++ app.js
@@ -1,49 +1,62 @@
 const express = require('express');
 const app = express();
-const { Client, TablesDB, Permissions, Role} = require('appwrite');
+let { Client, TablesDB, Permission, Role, Account, ID} = require('appwrite');
 require('dotenv').config();
+const port = process.env.PORT
+const cors = require('cors');
+
+app.use(cors())
 
 const client = new Client()
     .setEndpoint("https://fra.cloud.appwrite.io/v1")
     .setProject(process.env.APPWRITE_PROJECT_ID);
 
-const TablesDB = new TablesDB(client);
+TablesDB = new TablesDB(client);
 
 app.use(express.json());
 
 app.post('/api/application', async (req, res) => {
     const data = req.body;
-
+    console.log(data)
+    const stage = Array.isArray(data.stage) ? data.stage : [data.stage];
+    const salary = parseInt(data.salary, 10);
     try {
-        let promise = TablesDB.createRow(
-            process.env.APPWRITE_DATABASE_ID,
-            process.env.APPWRITE_COLLECTION_ID,
-            {
+        let promise = TablesDB.createRow({
+            databaseId: process.env.APPWRITE_DATABASE_ID,
+            tableId: process.env.APPWRITE_COLLECTION_ID,
+            rowId: ID.unique(),
+            data: {
                 title: data.title,
                 company: data.company,
                 date_applied: data.date_applied,
                 last_interacted: data.last_interacted,
-                status: data.status,
+                stage: stage,
                 description: data.description,
                 place: data.place,
-                salary: data.salary,
-                key_tasks: data.key_tasks
+                salary: salary,
+                key_task: data.key_tasks,
+                userId: data.userId,
             },
-            [
-                Permissions.read(Role.user(data.userId)),
-                Permissions.update(Role.user(data.userId)),
-                Permissions.delete(Role.user(data.userId))
-            ]
-        )
+            permission: [
+                Permission.read(Role.user(data.userId)),
+                Permission.update(Role.user(data.userId)),
+                Permission.delete(Role.user(data.userId)),
+            ],
+        });
         promise.then(function(response){
             console.log(response)
             if (response.status === 201){
                 return res.status(201).send("Added Successfully")
             }
         }, function (error) {
+            res.status(error.code).json(error.response)
             console.error(error)
         })
-    } catch {
-        console.error('vgz')
+    } catch (error) {
+        console.error(error)
     }
 })
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
--- public/script.js
+++ public/script.js
@@ -1,4 +1,16 @@
-document.getElementById('year').textContent = new Date().getFullYear();
+const client = new Appwrite.Client()
+    .setEndpoint("https://fra.cloud.appwrite.io/v1")
+    .setProject("690e41bb001b23b7fce1");
+
+const account = new Appwrite.Account(client)
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById("year").innerHTML = new Date().getFullYear();
+    // if (!window.location.href.includes('index') && window.location.pathname !== '/') {
+    //     checkAuthStatus();
+    // }
+})
 
 
 function showLoginForm() {
@@ -7,17 +19,51 @@ function showLoginForm() {
 }
 
 function closeLoginForm(){
-    
+
 }
 
-function login(){
+async function checkAuthStatus() {
+    try {
+        const user = await account.get();
+        console.log("User is authenticated:", user);
+        // Proceed with your authenticated app flow
+        return user;
+    } catch (error) {
+        console.error("User is not authenticated:", error);
+        window.location.href = '/';
+        return null;
+    }
+}
 
+async function login(){
+    try {
+        const result = await account.createEmailPasswordSession(
+            document.getElementById('email').value,
+            document.getElementById('password').value
+        );
+        sessionStorage.setItem('userId', result.userId);
+        window.location.href = '/overview.html';
+    } catch (error) {
+        console.error("Failed to login:", error);
+        alert("Login failed. Please check your credentials or network connection.");
+    }
+}
+
+function register(){
+    const email = document.getElementById('regiEmail').value;
+    const password = document.getElementById('regiPass').value
+    account.create({
+        email: email, 
+        password: password
+    });
+    console.log(user);
 }
 
 
 
 function addApplication(event) {
     event.preventDefault();
+    checkAuthStatus();
     const title = document.getElementById("title").value;
     const company = document.getElementById("company").value;
     const date_applied = new Date().toISOString().split("T")[0];
@@ -29,21 +75,24 @@ function addApplication(event) {
     const tasks = document.getElementById("key_tasks").value;
     const key_tasks = tasks.split(";").map(task => task.trim()).filter(task => task.length > 0);
 
-    console.log({
-        title,
-        company,
-        date_applied,
-        last_interacted,
-        status,
-        description,
-        place,
-        salary,
-        key_tasks
-    });
-    fetch('/api/applications', {
-        method: 'POST',
+    console.log(
+        {
+            title,
+            company,
+            date_applied,
+            last_interacted,
+            status,
+            description,
+            place,
+            salary,
+            key_tasks,
+        },
+        sessionStorage.getItem("userId")
+    );
+    fetch("http://localhost:3050/api/application", {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({
             title,
@@ -55,18 +104,19 @@ function addApplication(event) {
             place,
             salary,
             key_tasks,
-            userId: sessionStorage.getItem('userId')
+            userId: sessionStorage.getItem("userId"),
+        }),
+    })
+        .then((response) => {
+            if (response.ok) {
+                alert("Application added successfully!");
+                window.location.href = "index.html";
+            } else {
+                alert("Failed to add application.");
+            }
         })
-    }).then(response => {
-        if (response.ok) {
-            alert('Application added successfully!');
-            window.location.href = 'index.html';
-        } else {
-            alert('Failed to add application.');
-        }
-    }).catch(error => {
-        console.error('Error:', error);
-        alert('An error occurred while adding the application.');
-    }
-    )
+        .catch((error) => {
+            console.error("Error:", error);
+            alert("An error occurred while adding the application.");
+        });
 }
