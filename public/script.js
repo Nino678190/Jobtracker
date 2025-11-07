53efb241b200e23777d8c4cb911b95484692a55a
Nino678190 <superwauzi@gmx.de>
2025-11-07 20:14:21 +0100

    Initial project setup with basic structure, including HTML, CSS, JavaScript, and configuration files.

---
+++ public/script.js
@@ -0,0 +1,8 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+
+
+function showLoginForm() {
+    const loginDialog = document.getElementById('loginDialog');
+    loginDialog.style.display = 'flex';
+}
+
